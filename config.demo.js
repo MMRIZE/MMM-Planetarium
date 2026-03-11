8af2c6acfeafa15ca2b6921723832b99d7a80f1f
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2026-03-11 20:47:08 +0100

    chore: add demo config

---
+++ config.demo.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-undef */
+const config = {
+	address: "localhost",
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"],
+	logLevel: ["INFO", "LOG", "WARN", "ERROR", "DEBUG"],
+
+	modules: [
+		{
+			module: "clock",
+			position: "top_left",
+			config: {
+				displaySeconds: false
+			}
+		},
+		{
+			module: "MMM-Planetarium",
+			position: "fullscreen_below",
+			config: {
+				width: "100vw",
+				height: "100vh",
+				latitude: 50.0836,  // Ihre Position
+				longitude: 8.4694,
+				constellations: true,
+				showplanets: true,
+				live: true,
+				paninterval: 500,
+				panoffset: -0.1
+			}
+		},
+	]
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") { module.exports = config; }
